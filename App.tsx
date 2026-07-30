8f832dfa66b4e51f9f220925603028628b041665
kbappsdevelopment <kbus15155@gmail.com>
2026-07-30 12:50:07 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,13 @@
+<script async="async" data-cfasync="false" src="https://pl30599441.effectivecpmnetwork.com/69cfb4a84b7d0ce4e7c036040fca8267/invoke.js"></script>
+<div id="container-69cfb4a84b7d0ce4e7c036040fca8267"></div>
+
+<script>
+  atOptions = {
+    'key' : '435c93ee502e513d649b013818dbcfc0',
+    'format' : 'iframe',
+    'height' : 50,
+    'width' : 320,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/435c93ee502e513d649b013818dbcfc0/invoke.js"></script>
